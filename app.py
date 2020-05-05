11408d921a4206465594b7abbf529d3d5cc529d2
Admin <Admin@gmail.com>
2020-05-05 03:39:23 -0700

    Changed location of db import and decorator to new location.

--- app.py
+++ app.py
@@ -8,17 +8,11 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-from db import db
-
 app  = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'yahoo'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-	db.create_all()
 
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=1)
 jwt = JWT(app, authenticate, identity)   #jwt creates a new endpoint called /auth
@@ -30,5 +24,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+	from db import db
 	db.init_app(app)
 	app.run(port=5000, debug=True)
