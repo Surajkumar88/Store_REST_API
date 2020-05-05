0b4304ed5efb77ef4edb1fe073cd668af4d04fdc
Admin <Admin@gmail.com>
2020-05-05 01:20:32 -0700

    First commit, added REST API to access the items, stores and user authentication.

---
+++ resources/user.py
@@ -0,0 +1,28 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+
+	parser = reqparse.RequestParser()
+	parser.add_argument('username',
+		type= str,
+		required = True,
+		help = "This field is required"
+		)
+	parser.add_argument('password',
+		type= str,
+		required = True,
+		help = "This field is required"
+		)
+
+	def post(self):
+		data = UserRegister.parser.parse_args()
+
+		if UserModel.find_by_username(data['username']):
+			return {'message' : "username already exists!!!"}
+
+		user = UserModel(**data)  #**data is equal to data['username'], data['password']
+		user.save_to_db()
+
+		return {'message': 'User created successfully'}, 201
